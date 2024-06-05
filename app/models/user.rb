83d8b3d5fe0ab30ecea6dfe8797c8ec404af8cd2
hellonagi <38994662+hellonagi@users.noreply.github.com>
2024-06-05 19:49:01 +0900

    ユーザーモデルバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   # UserモデルとTodosアプリを関連付け(削除も同期)
   has_many :todos, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 32 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'maps#index'
+  get  '/signup', to: 'users#new'
 end
