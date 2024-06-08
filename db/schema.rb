50f0c1ec4f953164c4f83da3f99e63f4f1bc39ea
takuro <talangxiaotian6@gmail.com>
2024-06-09 02:36:41 +0900

    詳細画面の修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_07_112723) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_07_112723) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -74,6 +74,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_07_112723) do
     t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comment_id", null: false
+    t.index ["comment_id"], name: "index_todostates_on_comment_id"
     t.index ["todolist_id"], name: "index_todostates_on_todolist_id"
     t.index ["user_id"], name: "index_todostates_on_user_id"
   end
@@ -92,6 +94,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_07_112723) do
   add_foreign_key "comments", "users"
   add_foreign_key "todolists", "todos"
   add_foreign_key "todos", "users"
+  add_foreign_key "todostates", "comments"
   add_foreign_key "todostates", "todolists"
   add_foreign_key "todostates", "users"
 end
