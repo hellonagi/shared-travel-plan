933d449089645e489251139f70c58c31ba41606e
hellonagi <38994662+hellonagi@users.noreply.github.com>
2024-06-08 10:39:37 +0900

    初回ロード時に現在地を表示

--- app/javascript/custom/map.js
+++ app/javascript/custom/map.js
@@ -1,10 +1,8 @@
 async function initMap() {
-	console.log('AA2')
-
-	let map
+	let map, infoWindow
 
 	// The location of Uluru
-	const position = { lat: -25.344, lng: 131.031 }
+	const position = { lat: 35.681236, lng: 139.767125 }
 	// Request needed libraries.
 	//@ts-ignore
 	const { Map } = await google.maps.importLibrary('maps')
@@ -12,18 +10,55 @@ async function initMap() {
 
 	// The map, centered at Uluru
 	map = new Map(document.getElementById('map'), {
-		zoom: 4,
+		zoom: 6,
 		center: position,
 		mapId: 'DEMO_MAP_ID',
 		mapTypeControl: false,
+		streetViewControl: false,
 	})
 
+	infoWindow = new google.maps.InfoWindow()
+
+	{
+		;(() => {
+			// Try HTML5 geolocation.
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(
+					(position) => {
+						const pos = {
+							lat: position.coords.latitude,
+							lng: position.coords.longitude,
+						}
+
+						map.setCenter(pos)
+					},
+					() => {
+						handleLocationError(true, infoWindow, map.getCenter())
+					}
+				)
+			} else {
+				// Browser doesn't support Geolocation
+				handleLocationError(false, infoWindow, map.getCenter())
+			}
+		})()
+	}
+
 	// The marker, positioned at Uluru
-	const marker = new AdvancedMarkerView({
-		map: map,
-		position: position,
-		title: 'Uluru',
-	})
+	// const marker = new AdvancedMarkerView({
+	// 	map: map,
+	// 	position: position,
+	// 	title: 'Uluru',
+	// })
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos)
+	infoWindow.setContent(
+		browserHasGeolocation
+			? 'Error: The Geolocation service failed.'
+			: "Error: Your browser doesn't support geolocation."
+	)
+	infoWindow.open(map)
 }
 
 initMap()
