73f5e6a7c6f970625b9dcd1c7870f7e77065633f
hellonagi <38994662+hellonagi@users.noreply.github.com>
2024-06-09 01:54:58 +0900

    不要なコントローラを削除

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  post 'search', to: 'maps#search', as: 'search_places'
 
   resources :comments, only: %i[index show]
 end
